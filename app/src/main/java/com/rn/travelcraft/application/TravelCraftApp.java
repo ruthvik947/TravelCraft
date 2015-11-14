f3427dd0d0fbac2a6682d360fc93be8a13c96cad
Nakul Camasamudram <nakul.cr7@gmail.com>
2015-11-14 15:14:57 +0530

    - UI Changes

--- app/src/main/java/com/rn/travelcraft/application/TravelCraftApp.java
+++ app/src/main/java/com/rn/travelcraft/application/TravelCraftApp.java
@@ -5,6 +5,7 @@ import android.app.Application;
 import com.facebook.FacebookSdk;
 import com.parse.Parse;
 import com.parse.ParseFacebookUtils;
+import com.parse.ParseTwitterUtils;
 
 
 public class TravelCraftApp extends Application {
@@ -24,6 +25,8 @@ public class TravelCraftApp extends Application {
         //Initialize Parse Facebook integration
         ParseFacebookUtils.initialize(getApplicationContext());
 
+        //Parse Twitter integration
+        ParseTwitterUtils.initialize("vYwu1T9oQQ1u83l5nsgUmYLBq", "ftmf1Pf6bpy8F3FaG8v7fsPbc9uo4w5QhT2Ubtr77qXvMEy8sv");
 
     }
 }
