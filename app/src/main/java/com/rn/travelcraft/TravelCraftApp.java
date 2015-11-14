86a76b9d178c6269a01eb11ca37e53fe9da85069
Ruthvik Peddawandla <ruthvik.ped94@gmail.com>
2015-11-13 20:19:18 -0800

    Init. commit

---
+++ app/src/main/java/com/rn/travelcraft/TravelCraftApp.java
@@ -0,0 +1,23 @@
+package com.rn.travelcraft;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+
+public class TravelCraftApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Parse initialization/permissions
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this, "Qt1kfx56DpzLgywLINf43f0nZAycmqVdSnOUBRfl", "pW9Z1oiQ7lBRYFweKfOECRtJJQmw3KcAMeUkJqVK");
+
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
+    }
+}
