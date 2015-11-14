c3f5ac42647afc721862505a204f3f54856f628b
Nakul Camasamudram <nakul.cr7@gmail.com>
2015-11-14 22:28:03 +0530

    - Added Nav Drawer to HomeActivity

--- app/src/main/java/com/rn/travelcraft/activities/HomeActivity.java
+++ app/src/main/java/com/rn/travelcraft/activities/HomeActivity.java
@@ -1,13 +1,18 @@
 package com.rn.travelcraft.activities;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import com.parse.ParseUser;
 import com.rn.travelcraft.R;
@@ -17,16 +22,37 @@ public class HomeActivity extends AppCompatActivity {
 
     public static final String TAG = HomeActivity.class.getSimpleName();
 
+    private DrawerLayout mDrawer;
+    private ActionBarDrawerToggle mDrawerToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
 
         Log.d(TravelCraftApp.TAG, "HomeActivity onCreate()");
 
-        setContentView(R.layout.activity_home);
+        ParseUser currentUser = ParseUser.getCurrentUser();
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("TravelCraft");
+
+        mDrawer = (DrawerLayout) findViewById(R.id.parent);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawer, toolbar, R.string.drawer_open,
+                R.string.drawer_close);
+        mDrawer.setDrawerListener(mDrawerToggle);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation);
+        setupDrawerContent(navigationView);
+
+        View headerView = navigationView.inflateHeaderView(R.layout.nav_header);
+        TextView userName = (TextView) headerView.findViewById(R.id.user_name);
+        TextView userEmail = (TextView) headerView.findViewById(R.id.user_email);
+        if (currentUser != null) {
+            userName.setText(currentUser.getString("name"));
+            userEmail.setText(currentUser.getEmail());
+        }
     }
 
     @Override
@@ -36,20 +62,18 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home, menu);
-        return true;
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
 
         switch (item.getItemId()) {
+            case android.support.v7.appcompat.R.id.home:
+                mDrawer.openDrawer(GravityCompat.START);
+                return true;
             case R.id.action_settings:
                 return true;
             case R.id.action_logout:
@@ -59,6 +83,49 @@ public class HomeActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggles
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    public void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        selectDrawerItem(menuItem);
+                        return true;
+                    }
+                });
+    }
+
+    public void selectDrawerItem(MenuItem menuItem) {
+
+        switch (menuItem.getItemId()) {
+            case R.id.nav_profile:
+                break;
+            case R.id.nav_about:
+                break;
+            case R.id.nav_logout:
+                logout();
+                break;
+            default:
+                //fragmentClass = FirstFragment.class;
+        }
+
+        menuItem.setChecked(true);
+        mDrawer.closeDrawers();
     }
 
     private void logout() {
